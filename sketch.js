1da07383d596d3c6b8cf2a0c86f409da270a3b65
Dudinhasales <00001119652741sp@al.educacao.sp.gov.br>
2024-08-06 17:01:53 -0300

    Arquivo.css

---
+++ sketch.js
@@ -0,0 +1,81 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  }let palavra;
+  
+  function setup() {
+    createCanvas(400, 400);
+    
+    
+    let palavras = ["Dudinha", "Duda", "Dudu"];
+    
+    palavra = random(palavras);
+  }
+  
+  function inicializaCores() {
+    
+    background("#9C27B0");
+    fill("#9E9E9E");
+    textSize(64);
+    textAlign(CENTER, CENTER);
+  }
+  
+  function draw() {
+    
+    inicializaCores();
+  
+    let maximo = width;
+    let minimo = 0;
+    // mouseX, 0, width ==> 0, palavra.length
+    
+    let quantidade = map(mouseX, 0, width, 1, palavra.length);
+    //console.log(quantidade);
+    let parcial = palavra.substring(0, quantidade);
+    text(parcial, 200, 200);
+    
+  }
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}let palavra;
+
+function setup() {
+  createCanvas(400, 400);
+  
+  
+  let palavras = ["Dudinha", "Duda", "Dudu"];
+  
+  palavra = random(palavras);
+}
+
+function inicializaCores() {
+  
+  background("#9C27B0");
+  fill("#9E9E9E");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+}
+
+function draw() {
+  
+  inicializaCores();
+
+  let maximo = width;
+  let minimo = 0;
+  // mouseX, 0, width ==> 0, palavra.length
+  
+  let quantidade = map(mouseX, 0, width, 1, palavra.length);
+  //console.log(quantidade);
+  let parcial = palavra.substring(0, quantidade);
+  text(parcial, 200, 200);
+  
+}
+
+      
+}
